40c82622ebfe9864267d25bc8216cf1a1fe46228
yashUI <yashkr10316@gmail.com>
2025-06-19 16:09:07 +0530

    skills update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
-import Home from './components/pages/home'; // Import the Home page
+import Home from './components/pages/home';
 import Project from './components/pages/project';
 import About from './components/pages/about';
 import Skill from './components/pages/skill';
 import Blog from './components/pages/blog';
 import Contact from './components/pages/contact';
 import Resource from './components/pages/resource';
+import UnderDevelopment from './components/underdevpg/Under-development';
 import Footer from './components/footer/footer';
 import './App.scss';
 
@@ -15,33 +16,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar /> {/* Navbar will appear on all pages */}
+        <Navbar />
         <Routes>
-          {/* Home page route */}
           <Route path="/" element={<Home />} />
-          
-           {/* Project page route */}
-           <Route path="/project" element={<Project />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skill" element={<Skill />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/resource" element={<Resource />} />
+          <Route path="/contact" element={<Contact />} />
 
-           {/* About page route */}
-           <Route path="/about" element={<About />} />
-
-           {/* Skill page route */}
-           <Route path="/skill" element={<Skill />} />
-
-           {/* Blog page route */}
-           <Route path="/blog" element={<Blog />} />
-
-           {/* Blog page route */}
-           <Route path="/resource" element={<Resource />} />
-
-           {/* Contact page route */}
-           <Route path="/contact" element={<Contact />} />
-
-          {/* Add more routes for other pages here */}
-          {/* Example: <Route path="/about" element={<About />} /> */}
+          {/* ✅ Fallback Route */}
+          <Route path="*" element={<UnderDevelopment />} />
         </Routes>
-        <Footer /> {/* Footer will appear on all pages */}
+        <Footer />
       </div>
     </Router>
   );
